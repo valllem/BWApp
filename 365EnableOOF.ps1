02c80ee0c526c5ae6a5ea6a45ab6e2885820877d
valllem <55220631+valllem@users.noreply.github.com>
2019-09-16 12:57:07 +1000

    Version 0.1.2
    Added 365 AutoReply options to enable or disable out of office.

---
+++ 365EnableOOF.ps1
@@ -0,0 +1,21 @@
+﻿Clear-Host
+
+Write-Host -ForegroundColor Yellow 'Enabling Out of Office Reply'
+Write-Host -ForegroundColor Yellow ''
+Write-Host -ForegroundColor Yellow ''
+Write-Host -ForegroundColor Yellow ''
+Write-Host -ForegroundColor Yellow ''
+
+$UserIdentity = Read-Host 'Enter the Email of account to manage...'
+
+$AutoReplyMessage = Read-Host 'Please type your message and press enter...'
+
+Set-MailboxAutoReplyConfiguration -identity $UserIdentity -AutoReplyState Enabled -InternalMessage $AutoReplyMessage  -ExternalMessage $AutoReplyMessage
+
+
+
+Write-Host -ForegroundColor Green 'Out of Office Reply has been set'
+Write-Host -ForegroundColor Green 'Press any key to return to previous menu'
+$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
+Exit
---
+++ 365ListRoles.ps1
@@ -0,0 +1,30 @@
+﻿Clear-Host
+##--Script from: https://gist.github.com/ciphertxt/2036e614edf4bf920796059017fbbc3d--##
+
+Import-Module MSOline -EA 0
+
+Connect-MsolService -Credential (Get-Credential)
+
+$admins=@()
+
+$roles = Get-MsolRole 
+
+foreach ($role in $roles) {
+    $roleUsers = Get-MsolRoleMember -RoleObjectId $role.ObjectId
+
+    foreach ($roleUser in $roleUsers) {
+        $roleOutput = New-Object -TypeName PSObject
+        $roleOutput | Add-Member -MemberType NoteProperty -Name RoleMemberType -Value $roleUser.RoleMemberType
+        $roleOutput | Add-Member -MemberType NoteProperty -Name EmailAddress -Value $roleUser.EmailAddress
+        $roleOutput | Add-Member -MemberType NoteProperty -Name DisplayName -Value $roleUser.DisplayName
+        $roleOutput | Add-Member -MemberType NoteProperty -Name isLicensed -Value $roleUser.isLicensed
+        $roleOutput | Add-Member -MemberType NoteProperty -Name RoleName -Value $role.Name
+
+        $admins += $roleOutput
+    }
+}
+Write-Host -ForegroundColor Yellow 'Saving Export'
+$admins | Export-Csv -NoTypeInformation ~\Downloads\365RolesUsers.csv
+##--End of downloaded script--##
+$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
