556a041b9f86b988797c5e82cb3109ac24fc31d9
valllem <55220631+valllem@users.noreply.github.com>
2019-09-22 03:03:51 +1000

    Add files via upload

--- Launcher.ps1
+++ Launcher.ps1
@@ -3,8 +3,10 @@ New-Item -ItemType Directory -Force -Path C:\TEMP\BWApp
 New-Item -ItemType Directory -Force -Path C:\BWApp\Logs
 $TempPath = "C:\Temp\BWApp\Path.txt"
 Set-Content -Path $TempPath -value $Path
+Write-Host "$Path"
+write-host 'Loading App...'
 
 
-.\MENU.ps1
-
+C:\BWApp\BWApp-master\MENU.ps1
+Start-Sleep -Seconds 2
 exit
