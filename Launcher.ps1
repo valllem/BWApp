118775869b21df0a3bc68f26cae556309ec221ee
valllem <55220631+valllem@users.noreply.github.com>
2019-09-21 15:20:06 +1000

    Revised
    Added $Path to C:\BWApp\BWApp-master to fix launch issue.

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,9 +1,9 @@
-﻿$Path = Get-Location
+﻿$Path = "C:\BWApp\BWApp-master"
 New-Item -ItemType Directory -Force -Path C:\TEMP\BWApp
 New-Item -ItemType Directory -Force -Path C:\BWApp\Logs
 $TempPath = "C:\Temp\BWApp\Path.txt"
 Set-Content -Path $TempPath -value $Path
-
+cd $Path
 
 
 
