05788912ee2395ee11fa95954bf89ae281cd2c49
valllem <55220631+valllem@users.noreply.github.com>
2019-12-17 07:37:31 +1100

    Delete 365EnableAuditLog.ps1

--- 365EnableAuditLog.ps1
+++
@@ -1,6 +0,0 @@
-﻿Write-Host -ForegroundColor Yellow 'Enabling Audit Logging in 365...'
-Write-Host -ForegroundColor Yellow '=============================='
-Write-Host -ForegroundColor Yellow 'This can take several seconds'
-Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
-Write-Host -ForegroundColor Green 'Task Completed!'
-Write-Host -ForegroundColor Yellow '=============================='
