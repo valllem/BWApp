8c93863759fbdac616bf708f9677ea7b89c8d4ae
valllem <55220631+valllem@users.noreply.github.com>
2019-12-17 07:37:04 +1100

    Delete 365CheckAuditLog.ps1

--- 365CheckAuditLog.ps1
+++
@@ -1,9 +0,0 @@
-﻿write-Host -ForegroundColor Green '                AUDIT STATUS               '
-write-Host -ForegroundColor Green '==========================================='
-Get-AdminAuditLogConfig | FL UnifiedAuditLogIngestionEnabled
-write-Host -ForegroundColor Green '==========================================='
-write-Host -ForegroundColor Yellow 'Enabled status = UnifiedAuditLogIngestionEnabled : True'
-write-Host -ForegroundColor Yellow 'Disabled status = UnifiedAuditLogIngestionEnabled : False'
-write-Host -ForegroundColor Yellow ''
-write-Host -ForegroundColor Yellow '------------------------------------------'
-
