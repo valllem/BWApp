30a2ae8296bbf8be4b7b6136dcca5831ec9c9d0c
valllem <55220631+valllem@users.noreply.github.com>
2019-12-17 07:46:10 +1100

    deleted old

--- windowsUpdate.ps1
+++
@@ -1,8 +0,0 @@
-﻿Clear-Host
-Install-Module PSWindowsUpdate -Force
-Get-WindowsUpdate
-Install-WindowsUpdate -Force
-
-Write-Host -ForegroundColor Yellow 'Task Finished, press any key to return to previous menu...'
-$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
-$HOST.UI.RawUI.Flushinputbuffer()
